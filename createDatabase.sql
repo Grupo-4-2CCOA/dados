4235d25374a7e949adad4376595826336704c610
Fabricio-Prudente-Ferreira <fabricio.ferreira@sptech.school>
2025-03-15 22:18:46 -0300

    fix: Correção nos atributos

--- createDatabase.sql
+++ createDatabase.sql
@@ -7,33 +7,33 @@ use `grupo4`;
 -- tabela de usuários:
 create table user (
   `id` int primary key auto_increment,
-  `email` varchar(100) unique,
-  `password` varchar(50),
-  `phone` char(11),
-  `cpf` char(11) unique,
-  `name` varchar(75),
-  `cep` char(8),
+  `email` varchar(100) unique not null,
+  `password` varchar(50) not null,
+  `phone` char(11) unique not null,
+  `cpf` char(11) unique not null,
+  `name` varchar(75) not null,
+  `cep` char(8) not null,
   `birth` date,
-  `fidelity` enum('fiel', 'new')
+  `fidelity` enum('FIEL', 'NEW') not null
 );
 -- tabela de funcionários:
 create table employee (
   `id` int primary key auto_increment,
-  `email` varchar(100) unique,
-  `password` varchar(50),
-  `phone` char(11),
-  `cpf` char(11) unique,
-  `role` enum("owner", "employee"),
-  `cep` char(8),
+  `email` varchar(100) unique not null,
+  `password` varchar(50) not null,
+  `phone` char(11) unique not null,
+  `cpf` char(11) unique not null,
+  `role` enum("OWNER", "EMPLOYEE") not null,
+  `cep` char(8) not null,
   `birth` date,
   `name` varchar(75)
 );
 -- tabela de serviços:
 create table service (
   `id` int primary key auto_increment,
-  `type` varchar(50),
-  `category` varchar(75),
-  `price` decimal(5,2),
+  `type` varchar(50) not null,
+  `category` varchar(75) not null,
+  `price` decimal(5,2) not null,
   `discount` decimal(5,2),
   `description` varchar(200),
   `image` varchar(255)
@@ -41,12 +41,11 @@ create table service (
 -- tabela de agendamentos:
 create table schedule (
   `id` int primary key auto_increment,
-  `day` date,
-  `time` time,
+  `date_time` datetime not null,
   `duration` varchar(50),
   `fk_user` int,
-  `fk_servico` int,
-  `fk_funcionario` int,
+  `fk_service` int,
+  `fk_employee` int,
 
   constraint `schedule_fk_user` foreign key (`fk_user`) references `user`(`id`),
   constraint `schedule_fk_employee` foreign key (`fk_employee`) references `employee`(`id`),
@@ -55,8 +54,8 @@ create table schedule (
 -- tabela de métodos de pagamentos:
 create table payment(
   `id` int primary key auto_increment,
-  `type` enum('debito', 'credito', 'pix'),
-  `date` datetime,
+  `type` enum('DEBITO', 'CREDITO', 'PIX') not null,
+  `date_time` datetime not null,
   `transaction` varchar(255),
   `fk_user` int,
   
@@ -66,8 +65,11 @@ create table payment(
 create table feedback (
   `id` int primary key auto_increment,
   `comment` varchar(200),
-  `rating` enum (1, 2, 3, 4, 5),
+  `rating` tinyint,
   `fk_user` int,
+  `fk_schedule` int,
   
-  constraint `feedback_fk_user` foreign key (`fk_user`) references `user`(`id`)
+  constraint `feedback_check_rating` check(`rating` between 0 and 5),
+  constraint `feedback_fk_user` foreign key (`fk_user`) references `user`(`id`),
+  constraint `feedback_fk_schedule` foreign key (`fk_schedule`) references `schedule`(`id`)
 );
