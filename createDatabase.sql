d18eb9777c7240f4dec0be19aff07b17fb791962
LucasNagasse <lucas.nagasse@sptech.school>
2025-04-10 20:32:19 -0300

    feature: alterando o script de criação do banco de dados

--- createDatabase.sql
+++ createDatabase.sql
@@ -5,84 +5,100 @@ create database if not exists `grupo4`;
 use `grupo4`;
 
 -- tabela de usuários:
-create table user (
-  `id` int primary key auto_increment,
-  `email` varchar(100) unique not null,
-  `password` varchar(50) not null,
-  `phone` char(11) unique not null,
-  `cpf` char(11) unique not null,
-  `name` varchar(75) not null,
-  `cep` char(8) not null,
-  `birth` date
-);
--- tabela de funcionários:
-create table employee (
-  `id` int primary key auto_increment,
-  `email` varchar(100) unique not null,
-  `password` varchar(50) not null,
-  `phone` char(11) unique not null,
-  `cpf` char(11) unique not null,
-  `role` enum("OWNER", "INFRA", "EMPLOYEE") not null,
-  `cep` char(8) not null,
+create table `user`(
+  `id` int not null primary key,
+  `active` tinyint not null default 1,
+  `created_at` datetime not null default current_timestamp,
+  `updated_at` datetime not null default current_timestamp on update current_timestamp,
+
+  `role` enum("OWNER", "EMPLOYEE", "CUSTOMER") not null,
+  `name` varchar(80) not null,
+  `email` varchar(80) not null unique,
+  `password` varchar(80) not null,
+  `cpf` char(11) unique,
+  `phone` char(11) unique,
   `birth` date,
-  `name` varchar(75)
+  `cep` char(8)
+);
+-- tabela de tipo de pagamento:
+create table `payment_type`(
+  `id` int not null primary key,
+  `active` tinyint not null default 1,
+  `created_at` datetime not null default current_timestamp,
+  `updated_at` datetime not null default current_timestamp on update current_timestamp,
+
+  `name` varchar(80) not null,
+  `description` varchar(255)
+);
+-- tabela de categoria:
+create table `category`(
+  `id` int not null primary key,
+  `active` tinyint not null default 1,
+  `created_at` datetime not null default current_timestamp,
+  `updated_at` datetime not null default current_timestamp on update current_timestamp,
+
+  `name` varchar(80) not null,
+  `description` varchar(255)
 );
 -- tabela de serviços:
-create table service (
-  `id` int primary key auto_increment,
-  `type` varchar(50) not null,
-  `category` varchar(75) not null,
-  `price` decimal(5,2) not null,
-  `discount` decimal(5,2),
-  `description` varchar(200),
-  `image` varchar(255)
-);
--- tabela de atendimentos:
-create table appointment (
-	`id` int primary key auto_increment,
-    `status` enum("ACTIVE", "INACTIVE") not null,
-    `transaction_hash` varchar(255) not null,
-    `duraction` varchar(45),
-    `fk_employee` int,
-    `fk_service` int,
-    
-    constraint `appointment_fk_employee` foreign key (`fk_employee`) references `employee`(`id`),
-    constraint `appointment_fk_service` foreign key (`fk_service`) references `service`(`id`)
+create table `service`(
+  `id` int not null primary key,
+  `active` tinyint not null default 1,
+  `created_at` datetime not null default current_timestamp,
+  `updated_at` datetime not null default current_timestamp on update current_timestamp,
+
+  `name` varchar(80) not null,
+  `base_price` decimal(5,2) not null,
+  `base_duration` int not null,
+  `description` varchar(255),
+  `image` varchar(255),
+
+  `fk_category` int not null,
+
+  constraint `service_fk_category` foreign key (`fk_category`) references `category`(`id`)
 );
 -- tabela de agendamentos:
-create table schedule (
-  `id` int auto_increment primary key,
-  `status` enum("ACTIVE", "INACTIVE") not null,
-  `date_time` datetime not null,
-  `fk_appointment` int,
-  
-  constraint `schedule_fk_appointment` foreign key (`fk_appointment`) references `appointment`(`id`)
+create table `schedule` (
+  `id` int not null primary key,
+  `created_at` datetime not null default current_timestamp,
+  `updated_at` datetime not null default current_timestamp on update current_timestamp,
+
+  `status` enum('ACTIVE', 'COMPLETED', 'CANCELED') not null,
+  `appointment_datetime` datetime not null,
+  `duration` int null,
+  `transaction_hash` varchar(255),
+
+  `fk_user` int not null,
+  `fk_payment_type` int,
+
+  constraint `schedule_fk_user` foreign key (`fk_user`) references `user`(`id`),
+  constraint `schedule_fk_payment_type` foreign key (`fk_payment_type`) references `payment_type`(`id`)
 );
--- tabela de métodos de pagamentos:
-create table payment(
-  `id` int primary key auto_increment,
-  `type` varchar(50) not null,
-  `date_time` datetime not null,
-  `fk_appointment` int,
-  
-  constraint `payment_fk_appointment` foreign key (`fk_appointment`) references `appointment`(`id`)
+-- tabela de serviços por agendamento:
+create table `schedule_item`(
+  `id` int not null primary key,
+  `created_at` datetime not null default current_timestamp,
+  `updated_at` datetime not null default current_timestamp on update current_timestamp,
+
+  `final_price` decimal(5,2) not null,
+  `discount` decimal(5,2) not null,
+
+  `fk_schedule` int not null,
+
+  constraint `schedule_item_fk_schedule` foreign key (`fk_schedule`) references `schedule`(`id`)
 );
 -- tabela de feedbacks:
-create table feedback (
-  `id` int auto_increment primary key,
-  `comment` varchar(200),
-  `rating` int
-  
-  constraint `feedback_check_rating` check(`rating` between 0 and 5)
-);
--- tabela de n:n
-create table user_appointment (
-	`fk_user` int,
-    `fk_appointment` int,
-    `fk_feedback` int,
-    primary key(`fk_user`, `fk_appointment`, `fk_feedback`),
-    
-    constraint `user_appointment_fk_user` foreign key (`fk_user`) references `user`(`id`),
-    constraint `user_appointment_fk_appointment` foreign key (`fk_appointment`) references `appointment`(`id`),
-    constraint `user_appointment_fk_feedback` foreign key (`fk_feedback`) references `feedback`(`id`)
+create table `feedback` (
+  `id` int not null primary key,
+  `created_at` datetime not null default current_timestamp,
+  `updated_at` datetime not null default current_timestamp on update current_timestamp,
+
+  `rating` int not null,
+  `comment` varchar(255) null,
+
+  `fk_schedule` int not null,
+  `fk_user` int not null,
+
+  constraint `feedback_fk_schedule` foreign key (`fk_schedule`) references `schedule`(`id`),
+  constraint `feedback_fk_user` foreign key (`fk_user`) references `user`(`id`)
 );
