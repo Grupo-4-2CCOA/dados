71dd5c75052bb2b86bc3d2c096cd88c65436492a
LucasNagasse <lucas.nagasse@sptech.school>
2025-09-27 01:53:35 -0300

    fix: ajustes nas permissões dos perfis de usuário do banco de dados

--- mysql/createUser.sql
+++ mysql/createUser.sql
@@ -3,12 +3,12 @@
 -- Usuários para ambiente de desenvolvimento:
 drop user if exists 'grupo4-infra-dev'@'localhost';
 create user 'grupo4-infra-dev'@'localhost' identified by 'infra';
-grant select, insert, update, alter on grupo4.* to 'grupo4-infra-dev'@'localhost';
+grant all privileges, alter on grupo4.* to 'grupo4-infra-dev'@'localhost';
 
 -- Usuários para ambiente de homologação:
 drop user if exists 'grupo4-infra-hml'@'localhost';
 create user 'grupo4-infra-hml'@'localhost' identified by 'infra';
-grant select, insert, update, alter on grupo4.* to 'grupo4-infra-hml'@'localhost';
+grant all privileges, alter on grupo4.* to 'grupo4-infra-hml'@'localhost';
 
 -- Usuários para ambiente de produção:
 drop user if exists 'grupo4-infra-prd'@'localhost';
